9a8cc15429a830388ef71cd09ccc833f2fd370ee
lfturmina86 <lfturmina@gmail.com>
2025-02-26 10:47:33 -0300

    Delete ProjetoPWA directory

--- ProjetoPWA/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker instalado.");
-});
-
-self.addEventListener("fetch", (event) => {
-    console.log("Interceptando:", event.request.url);
-});
